1afec81c7a96ddf8d19bf882b3a027781b7fc446
IndiaJean-Jacques <78812857+IndiaJean-Jacques@users.noreply.github.com>
2021-04-04 13:02:43 +0100

    Add files via upload

---
+++ Sales by Match.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  3 22:16:39 2021
+
+@author: UKIAJ001
+"""
+ def sockMerchant(n, ar):
+    dictnocolor = {}
+    pairs = []
+    total = 0
+    for x in ar:
+        if x not in dictnocolor:
+            dictnocolor[x] = 1
+        else:
+            dictnocolor[x] = dictnocolor[x] + 1
+    for pair in dictnocolor:
+        pairs.append(dictnocolor[pair]//2)
+    for i in pairs:
+        total += i 
+    return total
